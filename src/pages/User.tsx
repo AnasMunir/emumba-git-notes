256458cb60bf015da7b22068f1ec19c4218209b2
Anas Munir <anas@ovou.com>
2024-03-01 18:38:46 +0500

    added pagination to user gists

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,33 +1,42 @@
 import { TGist, getUserGists } from "../api/gists";
 import { Link, LoaderFunctionArgs, useLoaderData, useParams } from "react-router-dom";
 import Card from "../components/Card";
+import Paginator from "../components/Paginator";
+import { TPaginationLinks } from "../utils/parseLinkHeaders";
 
 function User() {
-  const userGists = useLoaderData() as TGist[];
+  const { gists, paginationLinks } = useLoaderData() as { gists: TGist[]; paginationLinks: TPaginationLinks };
   const { userLogin } = useParams();
-  const userData = userGists[0].owner;
+  const userData = gists[0].owner;
   return (
     <div style={{ display: "flex" }}>
       <div>
         <img src={userData.avatar_url} alt={userLogin} height={260} width={260} style={{ borderRadius: "50%" }} />
       </div>
-      <div>
-        {userGists.map((gist) => (
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {gists.map((gist) => (
           <Card
             key={gist.id}
             url={gist.files[Object.keys(gist.files)[0]].raw_url}
             content={gist.files[Object.keys(gist.files)[0]].content}
             gistId={gist.id}
             userLogin={gist.owner.login}
+            showUserLink={false}
           />
         ))}
+        <div style={{ marginTop: "10px" }}>
+          <Paginator links={paginationLinks.links} />
+        </div>
       </div>
     </div>
   );
 }
 
-function loader({ params, request: { signal } }: LoaderFunctionArgs) {
-  return getUserGists(params.userLogin!, signal);
+function loader({ params, request: { signal, url } }: LoaderFunctionArgs) {
+  const searchParams = new URL(url).searchParams;
+  const page = searchParams.get("page");
+  const perPage = searchParams.get("per_page");
+  return getUserGists(params.userLogin!, page, perPage, signal);
 }
 export const userRoute = {
   element: <User />,

c899ba5e72585a750ecc5d6e4e11ec012370479a
Anas Munir <anas@ovou.com>
2024-03-01 14:44:28 +0500

    created paginator component

---
+++ src/components/Paginator.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { TPaginationLinks } from "../utils/parseLinkHeaders";
+
+function Paginator({ links }: TPaginationLinks) {
+  const { prev, first, last, next } = links;
+  return (
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <LinkButton pageLink={first} linkText='First' />
+      <LinkButton pageLink={prev} linkText='Prev' />
+      <LinkButton pageLink={next} linkText='Next' />
+      <LinkButton pageLink={last} linkText='Last' />
+    </div>
+  );
+}
+
+function LinkButton({ pageLink, linkText }: { pageLink?: string; linkText: string }) {
+  return (
+    <button className='btn' disabled={!pageLink}>
+      <Link style={{ textDecoration: "none", pointerEvents: pageLink ? "all" : "none" }} to={`?${pageLink}`}>
+        {linkText}
+      </Link>
+    </button>
+  );
+}
+
+export default Paginator;

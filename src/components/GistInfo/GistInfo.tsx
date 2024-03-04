b59424e7026ea8d37a8317ac4704df085b75faf7
Anas Munir <anas@ovou.com>
2024-03-04 15:51:00 +0500

    added links to gistinfo component

--- src/components/GistInfo/GistInfo.tsx
+++ src/components/GistInfo/GistInfo.tsx
@@ -1,29 +1,34 @@
+import { Link } from "react-router-dom";
 import Avatar from "../Avatar";
 import "./styles.css";
 
 interface IGistInfo {
+  gistId: string;
   username: string;
   filename: string;
   createdAt: string;
 }
-function GistInfo({ username, filename, createdAt }: IGistInfo) {
+function GistInfo({ gistId, username, filename, createdAt }: IGistInfo) {
   const date = new Date(createdAt);
   const formattedDateTime = new Intl.DateTimeFormat("en-GB", {
     dateStyle: "full",
   }).format(date.toString() === "Invalid Date" ? undefined : date);
 
   return (
-    <div className='gist-info'>
+    <div className='gist'>
       <div>
         <Avatar src='https://avatars.githubusercontent.com/u/8572727?v=4' alt='anas' />
       </div>
-      <div className='title'>
+      <div className='info'>
         {username && filename && (
-          <p>
-            {username} / <span id='filename'>{filename}</span>
+          <p className='title'>
+            <Link to={`/${username}`}>{username} / </Link>
+            <Link to={`/gists/${gistId}`}>
+              <span> {filename}</span>
+            </Link>
           </p>
         )}
-        {date.toString() !== "Invalid Date" && <p>Create {formattedDateTime}</p>}
+        <div className='subtitle'>{date.toString() !== "Invalid Date" && <p>Create {formattedDateTime}</p>}</div>
       </div>
     </div>
   );

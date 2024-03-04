4d25ea912218c8942380c554b597c0360d89532e
Anas Munir <anas@ovou.com>
2024-03-04 18:17:23 +0500

    adding list view to gists page

--- src/pages/Gists.tsx
+++ src/pages/Gists.tsx
@@ -1,24 +1,35 @@
-import { Link, LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import { TGist, getPublicGists } from "../api/gists";
 import Card from "../components/Card/Card";
 import { TPaginationLinks } from "../utils/parseLinkHeaders";
 import Paginator from "../components/Paginator";
+import { useState } from "react";
 
-// TODO: create styles and sub components
 function Gists() {
   const { gists, paginationLinks } = useLoaderData() as { gists: TGist[]; paginationLinks: TPaginationLinks };
+  const [displayFormat, setDisplayForm] = useState<"cards" | "list">("cards");
   return (
     <>
-      {/* <div className='card-grid'>
-        <Link className='btn btn-outline' to='new'>
-          New
-        </Link>
-        {gists.map((gist) => (
-          <Card key={gist.id} gist={gist} />
-        ))}
-      </div> */}
-      <Card key={gists[0].id} gist={gists[0]} />
-      <div style={{ marginTop: "10px" }}>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          style={{ borderRight: "1px solid lightgray", padding: "0.5rem", cursor: "pointer" }}
+          onClick={() => setDisplayForm("cards")}>
+          Card
+        </p>
+        <p style={{ padding: "0.5rem", cursor: "pointer" }} onClick={() => setDisplayForm("list")}>
+          List
+        </p>
+      </div>
+      {displayFormat === "cards" ? (
+        <div className='card-grid'>
+          {gists.map((gist) => (
+            <Card key={gist.id} gist={gist} />
+          ))}
+        </div>
+      ) : (
+        <div></div>
+      )}
+      <div style={{ marginTop: "20px" }}>
         <Paginator links={paginationLinks.links} />
       </div>
     </>

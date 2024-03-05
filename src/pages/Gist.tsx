64e70ba4b118da9faf958fbd4f0519171e50ae19
Anas Munir <anas.munir.92@gmail.com>
2024-03-05 18:33:57 +0500

    created icon buttons

--- src/pages/Gist.tsx
+++ src/pages/Gist.tsx
@@ -3,7 +3,9 @@ import { TGist, deleteGist, getGist } from "../api/gists";
 import Card from "../components/Card/Card";
 import { useContext } from "react";
 import { UserContext } from "../App";
-import GistInfo from '../components/GistInfo/GistInfo';
+import GistInfo from "../components/GistInfo/GistInfo";
+import EditIcon from "../components/icons/EditIcon";
+import DeleteIcon from "../components/icons/DeleteIcon";
 
 function Gist() {
   const gist = useLoaderData() as TGist;
@@ -11,19 +13,7 @@ function Gist() {
 
   return (
     <>
-      {gist.owner.id === id && (
-        <div style={{ display: "flex" }}>
-          <Link to='edit' className='btn'>
-            Edit
-          </Link>
-          <Form method='delete' replace>
-            <button className='btn' type='submit'>
-              Delete
-            </button>
-          </Form>
-        </div>
-      )}
-      <div style={{ margin: "15px 0px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between", margin: "15px 0px" }}>
         <GistInfo
           src={gist.owner.avatar_url}
           gistId={gist.id}
@@ -31,6 +21,22 @@ function Gist() {
           filename={gist.files[Object.keys(gist.files)[0]].filename}
           createdAt={gist.created_at}
         />
+        {gist.owner.id === id && (
+          <div style={{ display: "flex" }}>
+            <Link to='edit'>
+              <button className='icon-btn'>
+                <EditIcon />
+                Edit
+              </button>
+            </Link>
+            <Form method='delete' replace>
+              <button className='icon-btn' type='submit'>
+                <DeleteIcon />
+                Delete
+              </button>
+            </Form>
+          </div>
+        )}
       </div>
       <Card gist={gist} />
     </>

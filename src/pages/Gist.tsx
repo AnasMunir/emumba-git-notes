91f21209fd2fe52c812650c1118fb716507389b3
Anas Munir <anas.munir.92@gmail.com>
2024-03-05 17:30:59 +0500

    added gistinfo to gist page

--- src/pages/Gist.tsx
+++ src/pages/Gist.tsx
@@ -3,6 +3,7 @@ import { TGist, deleteGist, getGist } from "../api/gists";
 import Card from "../components/Card/Card";
 import { useContext } from "react";
 import { UserContext } from "../App";
+import GistInfo from '../components/GistInfo/GistInfo';
 
 function Gist() {
   const gist = useLoaderData() as TGist;
@@ -22,8 +23,15 @@ function Gist() {
           </Form>
         </div>
       )}
-      <br />
-      <br />
+      <div style={{ margin: "15px 0px" }}>
+        <GistInfo
+          src={gist.owner.avatar_url}
+          gistId={gist.id}
+          username={gist.owner.login}
+          filename={gist.files[Object.keys(gist.files)[0]].filename}
+          createdAt={gist.created_at}
+        />
+      </div>
       <Card gist={gist} />
     </>
   );
